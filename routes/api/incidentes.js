8f9bda740097bf9caffdac58e23165d324fbbeb2
Maria Jose Castro <mariajose199850@hotmail.com>
2019-07-09 19:34:42 -0600

    PUT AND DELETE

--- routes/api/incidentes.js
+++ routes/api/incidentes.js
@@ -31,7 +31,7 @@ function initIncidentes(db){
             "descripcion":"",
             "fechaYHora":new Date().getTime(),
             "tipo":"" ,
-            "estado":"" ,
+            "estado":"",
             "usuarioRegistra":"",
             "usuarioAsignado":"",
             "fechaHoraAsignado": new Date().getTime(),
@@ -51,6 +51,44 @@ function initIncidentes(db){
                  return res.status(200).json(rslt.ops[0]);
          });
      }); 
+
+     router.put('/asign/:id', (req,res,next) =>{
+        var query = {"_id": new ObjectID(req.params.id)};
+        var update = {"$inc":{"fechaHoraAsignado": new Date().getTime()}};
+
+        incidentesColl.updateOne(query, update, (err,rslt)=>{
+            if(err){
+                console.log(err);
+                return res.status(404).json({"error": "No se puede modificar"});
+            }
+           return res.status(200).json(rslt);
+        })
+    });
+
+    router.put('/close/:id', (req,res,next) =>{
+        var query = {"_id": new ObjectID(req.params.id)};
+        var update = {"$inc":{"fechaHoraCerrado": new Date().getTime()}};
+
+        incidentesColl.updateOne(query, update, (err,rslt)=>{
+            if(err){
+                console.log(err);
+                return res.status(404).json({"error": "No se puede modificar"});
+            }
+           return res.status(200).json(rslt);
+        })
+    });
+
+    router.delete('/:id', (req,res,next) =>{
+        var query = {"_id": new ObjectID(req.params.id)};
+
+        incidentesColl.removeOne(query, (err,rslt)=>{
+            if(err){
+                console.log(err);
+                return res.status(404).json({"error": "No se puedo borrar"});
+            }
+           return res.status(200).json(rslt);
+        })
+    });
     return router;
 }
 module.exports = initIncidentes;
